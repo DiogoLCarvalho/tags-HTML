12555e54bed0968f6d1c5cd19eb4a92ac3fe3261
Diogo Lima Carvalho <diogo.carvalho.lc@gmail.com>
2021-05-27 10:33:34 -0300

    Subindo todos as tags feitas até agora

---
+++ HTML/atributos/js/main.js
@@ -0,0 +1,23 @@
+.botao{
+    color: black;
+    background-color: blanchedalmond;
+    border-radius: 10px;
+    padding: 10px 10px;
+    font-family: sans-serif;
+    text-decoration: none;
+    border: none;
+    font-size: 15px;
+}
+
+#quadrado{
+    width: 100px;
+    height: 100px;
+    background-color: brown;
+    text-align: center;
+}
+
+.drop{
+    width: 300px;
+    height: 300px;
+    border: cornflowerblue 15px solid;
+}
